4d67ce084e3c47c92caa89aa27ff6a0066b31e65
Kohsuke Kawaguchi <kk@kohsuke.org>
2008-05-27 01:05:26 +0000

    moving to groovy as view, instead of Jelly

---
+++ src/main/resources/torricelli/Torricelli/index.groovy
@@ -0,0 +1,75 @@
+import torricelli.Repository
+
+L l = taglib(L)
+
+List<Repository> repos = that.listRepositories();
+
+/**
+ * Generate a drop-down list of repositories
+ */
+def repoList = {
+    SELECT(NAME:"src",STYLE:"width:100%") {
+        repos.each { OPTION(it.name) }
+    }
+}
+
+l.layout {
+    l.left {
+        H2("Mercurial Repositories")
+
+        DIV(CLASS:"repositoryList") {
+            repos.each { repo ->
+                DIV {
+                    A(HREF:repo.name) {
+                        IMG(SRC:"img/package.gif",ALIGN:"middle")
+                    }
+                    A(HREF:repo.name,repo.name)
+                }
+            }
+        }
+    }
+
+    l.right {
+        DIV(CLASS:"box",STYLE:"width:auto;") {
+            H2("Create a new repository")
+            form("create") {
+                TR {
+                    TD("Name")
+                    TD { INPUT(TYPE:"text",NAME:"name") }
+                }
+            }
+
+            H2("Clone an existing repository")
+            form("clone") {
+                TR {
+                    TD("From")
+                    TD(repoList)
+                }
+                TR {
+                    TD("To")
+                    TD { INPUT(TYPE:"text",NAME:"name") }
+                }
+            }
+
+            H2("Delete a repository")
+            form("delete") {
+                TR {
+                    TD("Name")
+                    TD(repoList)
+                }
+            }
+        }
+    }
+}
+
+/**
+ * Defines a form in the box.
+ */
+def form(String name,Closure body) {
+    FORM(ACTION:name,METHOD:"post") {
+        TABLE(body)
+        DIV(ALIGN:"right") {
+            INPUT(TYPE:"submit",VALUE:name)
+        }
+    }
+}
